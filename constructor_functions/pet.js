91465b4b5e2537e8b1cffdcbcd3f7aba585d73ff
John <ashton.basile@gmail.com>
2023-08-08 13:36:56 +0100

    starter instructions for the pet task

---
+++ constructor_functions/pet.js
@@ -0,0 +1,12 @@
+/*
+Create a new file - pet.js
+Create a Pet constructor
+Assign values to the following properties in your Pet constructor via parameters:
+	name
+	species
+Add an eat method to your Pet's prototype. 
+This method should accept a food parameter and output 
+a string containing the name property and the food that was eaten. 
+For example: 'Scooby Doo ate a Scooby Snack'.
+*/
+
