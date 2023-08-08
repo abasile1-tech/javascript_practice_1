e6b6579b734de2d463f11b417ff391c6e7ff25cd
John <ashton.basile@gmail.com>
2023-08-08 11:09:20 +0100

    arrow functions can written on one line

---
+++ functions/arrow_functions.js
@@ -0,0 +1,9 @@
+var multiply = (firstNumber, secondNumber) => {
+	return firstNumber * secondNumber;
+}
+
+console.log('multiply 2 by 5:', multiply(2,5));
+
+var multiply2 = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+console.log('multiply 3 by 5:', multiply2(3,5));
