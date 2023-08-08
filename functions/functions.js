6dd4f00b6e39330f88646dd3ba5696a18cdf2905
John <ashton.basile@gmail.com>
2023-08-08 09:51:42 +0100

    I learned about default parameters

--- functions/functions.js
+++ functions/functions.js
@@ -3,3 +3,21 @@ function sayHello() {
 }
 
 console.log('sayHello message:', sayHello());
+
+function sayHello2(name) {
+	return `Hello ${name}!`;
+}
+
+console.log('sayHello2 message:', sayHello2('Danielle'));
+
+function sayHello3(name = 'World') {
+	return `Hello ${name}!`;
+}
+
+console.log('sayHello3 with no arguments:', sayHello3());
+
+function sayHello4(greeting, name = 'World') {
+	return `${greeting} ${name}!`;
+}
+
+console.log('sayHello4 message:', sayHello4('Hi', 'Danielle'));
