ce0a8aea40c3d569c4868fc1617f9abb9839b924
John <ashton.basile@gmail.com>
2023-08-08 12:18:19 +0100

    I got question 3 right

--- hw_scope_who_dunnit/episode3.js
+++ hw_scope_who_dunnit/episode3.js
@@ -10,3 +10,13 @@ console.log('First Verdict: ', firstVerdict);
 
 const secondVerdict = `The murderer is ${murderer}.`;
 console.log('Second Verdict: ', secondVerdict);
+
+/*
+I think that the output will be:
+  First Verdict: The murderer is Mrs. Peacock
+  Second Verdict: The murderer is Professor Plum
+
+The output actually was:
+  First Verdict:  The murderer is Mrs. Peacock.
+  Second Verdict:  The murderer is Professor Plum.
+*/
