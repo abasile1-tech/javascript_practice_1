61f8138de1c212e287f06e6ccce95d54db499a18
John <ashton.basile@gmail.com>
2023-08-09 15:20:06 +0100

    I finished writing the two functions in the higher order task practicing forEach

--- higher_order/task.js
+++ higher_order/task.js
@@ -11,3 +11,26 @@ Hint: If you get stuck, start by writing the function using a for of loop,
 then refactor, replacing the for loop with the forEach.
 */
 
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+const getEvens = function (numbers) {
+	const evens = [];
+	numbers.forEach((number) => {
+		if (number % 2 === 0) {
+			evens.push(number);
+		}
+	})
+	return evens;
+}
+
+console.log(getEvens(myNumbers));
+
+const sumElements = function (numbers) {
+	let total = 0;
+	numbers.forEach((number) => {
+		total += number;
+	})
+	return total;
+}
+
+console.log(sumElements(myNumbers));
