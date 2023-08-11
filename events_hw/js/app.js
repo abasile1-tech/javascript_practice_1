437c55d9899a415095ff7548c94ad35297c701c3
John <ashton.basile@gmail.com>
2023-08-11 14:55:16 +0100

    I wrote the handler functions for the button and the input

--- events_hw/js/app.js
+++ events_hw/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript has loaded');
+  const button = document.querySelector('#button');
+  button.addEventListener('click', handleButtonClick);
+
+  const textInput = document.querySelector('#input');
+  textInput.addEventListener('input', handleInput);
 });
+
+const handleButtonClick = function () {
+  const resultParagraph = document.querySelector('#button-result');
+  resultParagraph.textContent = 'That button has definitely been clicked.';
+}
+
+const handleInput = function (event) {
+  const resultParagraph = document.querySelector('#input-result');
+  resultParagraph.textContent = `You typed: ${event.target.value}`;
+}
