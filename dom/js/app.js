da743bb62e2b5bb917c276370887f7cc9a07c109
John <ashton.basile@gmail.com>
2023-08-11 11:17:56 +0100

    I finished the lesson up until the point that mentions modifying DOM elements

--- dom/js/app.js
+++ dom/js/app.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const title = document.querySelector('h1');
+	title.textContent = 'JavaScript says, Hello World!';
 
+	const welcomeParagraph = document.querySelector('#welcome-paragraph');
+
+	console.log(welcomeParagraph);
+	console.dir(welcomeParagraph);	
+
+	const redElement = document.querySelector('.red');
+	console.log(redElement);
+
+	const redListItem = document.querySelector('li.red');
+	console.dir(redListItem);
+
+	const allRedElements = document.querySelectorAll('li');
+	console.dir(allRedElements);
+});
