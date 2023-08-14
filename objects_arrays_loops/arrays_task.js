ce32034947a709b5abbeedbfa4f961b63db98350
John <ashton.basile@gmail.com>
2023-08-14 14:25:08 +0100

    some starter code for the arrays task

---
+++ objects_arrays_loops/arrays_task.js
@@ -0,0 +1,17 @@
+// ARRAY TASKS
+
+/* 
+	Task 1: Create an array called bookshelf containing a list of book titles
+*/
+
+// Task 2: Log the number of books on the bookshelf
+
+// Task 3: Log the second book on the bookshelf
+
+// Task 4: Replace the second book on the bookshelf with another book title
+
+// Task 5: Add a book to the end of the bookshelf
+
+// Task 6: Add a book to the beginning of the bookshelf
+
+// Task 7: Get the index of a book and remove it from the bookshelf
