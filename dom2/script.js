c79a4f3622046177700ad3d25d081b8c0f9884d6
John <ashton.basile@gmail.com>
2023-08-17 11:51:31 +0100

    classwork: playing around with javascript changing the dom

--- dom2/script.js
+++ dom2/script.js
@@ -8,4 +8,8 @@ console.log(window);
 console.log(document);
 
 const h1 = document.querySelector("h1");
-h1.textContent = "Hello World";
+h1.textContent = "Fringe Events!";
+
+const event_h3 = document.querySelector(".event h3");
+
+event_h3.textContent = "Shit-faced Rabbie Burns";
