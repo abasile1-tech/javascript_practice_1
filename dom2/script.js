6f1395737d821e47b5b0b69f78b3946c23534bf5
John <ashton.basile@gmail.com>
2023-08-17 11:38:49 +0100

    using defer script

--- dom2/script.js
+++ dom2/script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+const square = (num) => num * num;
+
+console.log(square(5));
+
+console.log(window);
+console.log(document);
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Hello World";
